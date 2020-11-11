51be73c71345560171ab40383c7a69fbfb9a3f8b
crispino480 <crispino480@users.noreply.github.com>
2020-11-11 22:17:50 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -14,31 +14,6 @@ int main (void){
 
         }while( height < 1 || height > 8  );
 
-                   /* for (int i = 0 ; i <= height ; i++)
-                          {
-                             for (int j = 0 ; j <= height ; j++){
-                                 if ( j > height - i ) {
-                                    printf("#");
-                                 }else{
-                                     printf(" ");
-                                 }
-                                 }
-                                printf("  ");
-
-                                 for (int k = 0 ; k < height ; k++){
-
-                                 if ( i <= k  ) {
-
-                                    printf(" ");
-                                 }else {
-                                     printf("#");
-                                    }
-
-                                 }
-                     printf("\n");
-                   }*/
-
-
                         for (int i = 1 ; i <= height ; i++)
                           {
                              for (int j = 1 ; j <= height ; j++){
@@ -47,23 +22,18 @@ int main (void){
                                  }else{
                                      printf(" ");
                                  }
-
-
                                  }
-
                                 printf("  ");
 
                                  for (int k = 1 ; k <= height ; k++){
-
                                  if ( i < k  ) {
-
                                     printf(" ");
+
                                  }else {
                                      printf("#");
                                     }
                                  }
                      printf("\n");
                    }
-
 }
 
