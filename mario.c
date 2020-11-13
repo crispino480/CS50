b0f32915538f2b97dd1ef2abbc1c04467be193b0
crispino480 <crispino480@users.noreply.github.com>
2020-11-13 02:27:02 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -15,8 +15,6 @@ printf("How tall is your pyramid\n");
 
     }while(height < 1 || height > 8);
 
-
-
  for (int i = 1 ; i <= height ; i++)
     {
     for (int j = 1 ; j <= height ; j++){
@@ -38,4 +36,7 @@ printf("How tall is your pyramid\n");
 }
 
 }
+
+
+
 
