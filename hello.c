59197eda9168397081203e41930df0947f15f190
crispino480 <crispino480@users.noreply.github.com>
2020-11-17 04:49:49 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void) {
+int main(void) 
+{
 
-    
+//ask for user name 
     string name = get_string("what is your name?\n");
 
     printf("hello,%s\n", name);
