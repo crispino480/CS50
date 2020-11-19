ffb04e5ffb5b5c9531b03b8234040b4f1d969fc7
crispino480 <crispino480@users.noreply.github.com>
2020-11-19 21:48:59 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,53 +1,36 @@
-// this version of Mario if feeling less comfortable
-
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
+int main(void)
+{
 //prompt for a number for the pyramid height
 
     int height;
 
-  do {
+    do
+    {
 
-printf("How tall is your pyramid\n");
+       printf("How tall is your pyramid\n");
         height = get_int("height: ");
 
-    }while(height < 1 || height > 8);
+    }
+    while (height < 1 || height > 8);
 
 
-/*
- for (int i = 1 ; i <= height ; i++)
+
+    for (int i = 0 ; i < height ; i++)
     {
-    for (int j = 1 ; j <= height ; j++){
-        if(j > height - i) {
-      printf("#");
-        }else{
-        printf(" ");
+for (int j = 1 ; j <= height ; j++)
+        {
+         if ( j >= height - i )
+            {
+             printf("#");
+             }
+            else
+            {
+             printf(" ");
+              }
          }
-    }
-    printf("  ");
-     for (int k = 1 ; k <= height ; k++){
-      if( i < k) {
-    printf(" ");
-        }else {
-    printf("#");
-         }
-       }
-     printf("\n");
-}*/
-
-for (int i = 1 ; i <= height ; i++)
-                          {
-      for (int j = 1 ; j <= height ; j++){
-        if(j > height - i) {
-      printf("#");
-        }else{
-        printf(" ");
-         }
-    }
-    printf("\n");
-  }
-
+         printf("\n");
+   }
 }
-
