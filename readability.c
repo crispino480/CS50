131fadb8e89b2f664e77db1ce913649ada48a233
crispino480 <crispino480@users.noreply.github.com>
2020-11-21 22:29:24 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -7,56 +7,97 @@
  string userInput();
  int lettercount(string t);
  int wordcount(string t);
+ int sentencecount(string t);
  int countl=0;
  int countw=0;
- char space = 0;
+ int counts=0;
+ string space = " ";
 
 
 int main (void)
 {
 
 string s = userInput(); //To get user input
+float lt = (float)lettercount(s);
+float w = (float)wordcount(s);
+float st = (float)sentencecount(s);
 
-printf("%i letter (s) \n",lettercount(s));
-printf("%i words (s) \n",wordcount(s));
+//index = 0.0588 * L - 0.296 * S - 15.8
+float L = (lt*100/w);
+float S = (st*100/w);
+
+int index = round(0.0588*L - 0.296*S - 15.8);
+
+if(index < 1)
+{
+ printf(" Before Grade 1\n");
+}else if (index >= 16)
+{
+printf(" Grade 16+ \n");
+}else
+{
+  printf(" Grade %i\n",index);
+}
+
 }
 
 
 
 
-//this function count the number of letters in a sentence
 
 
+
+//this function count the number of sentences in a text
+int sentencecount(string t)
+{
+  int n = strlen(t);
+
+  for(int i=0; i<n;i++)
+    {
+       if ( t[i] == '.' || t[i] == '!' || t[i] == '?')
+        {
+            counts++;// count letters
+        }
+    }
+    return counts;
+}
+
+
+
+//this function count the number of words in a sentence
 int wordcount(string t)
 {
   int n = strlen(t);
 
   for(int i=0; i<n;i++)
     {
-      if(!((t[i] >= 'a' && t[i] <= 'z' ) || (t[i] >= 'A' && t[i] <= 'Z'))  ) //check input non words
-      {
-       countw++; // count words
-      }
-
+       if (t[i] == ' ')
+        {
+            countw++;// count letters
+        }
     }
-    return countw;
+    return (countw + 1);
 }
 
 
 
+//this function count the number of letters in a sentence
 int lettercount(string t)
 {
 int n = strlen(t);
 
   for(int i=0; i<n;i++)
     {
-      if ( (t[i] >= 'a' && t[i] <= 'z' ) || (t[i] >= 'A' && t[i] <= 'Z')) // check if the input is a letter
+         if (isalpha(t[i])) // check if the input is a letter
         {
-           countl++;// count letters
-         }
+            countl++;// count letters
+        }
      }
      return countl;
  }
+
+
+
 
 
 //function to prompt for user input, return its value
