32d15b383d6d5033fca09edd63be46f155010831
crispino480 <crispino480@users.noreply.github.com>
2020-12-07 17:18:42 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -235,7 +235,7 @@ bool is_tie(int min)
 
     for(int i = 0; i < candidate_count ; i++)
     {
-     if(candidates[i].votes == min)
+     if((candidates[i].eliminated) && (candidates[i].votes == min))
      {
      return true;
      }
