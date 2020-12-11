c9c1c746fd7e8369fbf5b36ddb3bd46c8daf4b66
crispino480 <crispino480@users.noreply.github.com>
2020-12-11 00:37:16 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -25,6 +25,40 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    
+    for (int i = 0 , n = height; i < n; i++)
+    {
+       
+          // get the rgb pixel of the image, compute the average and assign the avg  value as new value to r,g,b
+         for (int j = 0 , m = width; j < m; j++)
+             {
+                 
+                 
+               int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtRed + .189 * image[i][j].rgbtRed;
+               int sepiaGreen = .349 * image[i][j].rgbtGreen + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtGreen;
+               int sepiaBlue = .272 * image[i][j].rgbtBlue + .534 * image[i][j].rgbtBlue + .131 * image[i][j].rgbtBlue;
+               
+               if(sepiaRed >255)
+               {
+                image[i][j].rgbtRed =255;
+               }else if(sepiaGreen >255)
+               {
+                image[i][j].rgbtGreen =255;
+               }else if(sepiaBlue >255)
+               {
+               image[i][j].rgbtBlue =255;
+               }else 
+               {
+                 image[i][j].rgbtRed = sepiaRed;
+                 image[i][j].rgbtGreen = sepiaGreen;
+                 image[i][j].rgbtBlue = sepiaBlue;
+               }
+             }
+                  
+                  
+         } 
+    
+
     return;
 }
 
