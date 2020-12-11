e282a29b7b0aad4b65cf41ba8459d541646daba0
crispino480 <crispino480@users.noreply.github.com>
2020-12-11 01:06:59 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -75,8 +75,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
          for (int j = 0 , m = width; j < m; j++)
              {
                 tmp = image[i][j].rgbtRed;
-                image[i][j].rgbtRed = image[i][n-1+j].rgbtRed;
-                image[i][n-1+j].rgbtRed = tmp;
+                image[i][j].rgbtRed = image[i][m-1+j].rgbtRed;
+                image[i][m-1+j].rgbtRed = tmp;
                 
              }
              
