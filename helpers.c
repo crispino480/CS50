01f5bdf5bb313ccc6a5c23ba05404b5a604f8a73
crispino480 <crispino480@users.noreply.github.com>
2020-12-11 00:17:27 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -5,24 +5,18 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int avg;
-    int j=0;
     
     for (int i = 0 , n = height; i < n; i++)
     {
-       do
-         {
+       
           // get the rgb pixel of the image, compute the average and assign the avg  value as new value to r,g,b
-         
-          avg = round((image[i][j].rgbtRed +  image[i][j].rgbtGreen +  image[i][j].rgbtBlue)/3.00);
-          image[i][j].rgbtRed =(int)avg;
-          image[i][j].rgbtGreen =(int)avg;
-          image[i][j].rgbtBlue =(int)avg;
-           
-         
-        j++;
-
-         } while (j < width);
-
+         for (int j = 0 , m = width; j < m; j++)
+             {
+              avg = round((image[i][j].rgbtRed +  image[i][j].rgbtGreen +  image[i][j].rgbtBlue)/3.00);
+              image[i][j].rgbtRed =(int)avg;
+              image[i][j].rgbtGreen =(int)avg;
+              image[i][j].rgbtBlue =(int)avg;
+         } 
     }
 
     return;
