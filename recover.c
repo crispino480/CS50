9a09b685f99ca41e33cfa60e186c4189cc5d37ac
crispino480 <crispino480@users.noreply.github.com>
2020-12-15 20:15:37 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
     }
 
 //open file
-    FILE *file = fopen(argv[1], "r");
+    FILE* ptr = fopen(argv[1], "r");
 
-    if (file == NULL)
+    if (ptr == NULL)
     {
         fprintf(stderr, "Unable to open file%s\n", argv[1]);
         return 1;
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 
 //read 3 bytes from file
     BYTE buffer[512];
-              fread(buffer, 512, 1, file);
+              fread(buffer, 512, 1, ptr);
 
     char *filename = malloc(sizeof(int) * 3);
             int count =  0;
          FILE *img = NULL;
 
 //check if bytes are 0xff 0xd8 0xff
-    while (fread(buffer, sizeof(BYTE), 1, file) || feof(file)==0)
+    while (fread(buffer, sizeof(BYTE), 1,ptr) || feof(ptr)==0)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 
 
     free(filename);
-    fclose(file);
+    fclose(ptr);
     return 0;
 }
 
