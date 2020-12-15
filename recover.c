ef10f7645b2a817ddaa9cb5c96fc121b3295c5a7
crispino480 <crispino480@users.noreply.github.com>
2020-12-15 03:17:08 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -31,17 +31,20 @@ fread (buffer, 512, 1, file);
 
 char *filename = malloc(sizeof(int)*3);
 int count = 0;
+FILE *img ;
 
 //check if bytes are 0xff 0xd8 0xff
+do
+{
 if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
 {
  sprintf(filename, "%03i.jpg" , count);
- FILE *img = fopen(filename, "w");
+img = fopen(filename, "w");
  fwrite(&buffer,sizeof(BYTE),1,img);
  fclose(img);
  count++;
 }
-
+}while (fread(buffer, sizeof(BYTE),1,img)!= EOF);
 
 free(filename);
 return 0;
