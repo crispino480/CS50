e666db1d8b530c2c8e70a8c26eafd5af4aa36455
crispino480 <crispino480@users.noreply.github.com>
2020-12-16 02:37:51 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -41,10 +41,10 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
 
-            if(img!=NULL)
+           /* if(img!=NULL)
             {
                 fclose(img);
-            }
+            }*/
 
             sprintf(filename, "%03i.jpg" , count);
             img = fopen(filename, "w");
