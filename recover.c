9dcab13c577d5f3307ff4aea5623d3eac10992a2
crispino480 <crispino480@users.noreply.github.com>
2020-12-17 04:22:16 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -31,7 +31,7 @@ FILE* outptr =NULL;
 int isFound =0;
 
 //while(fread(buffer,512, 1,inputptr) == 1)
-fread(buffer,sizeof(BYTE), 1,inputptr);
+fread(buffer,sizeof(buffer), 1,inputptr);
 while( fread(buffer,sizeof(buffer), 1,inputptr)==1) // Read blocks of 512 bytes, one at a time
 {
     if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) && (buffer[3] & 0xf0) == 0xe0)
