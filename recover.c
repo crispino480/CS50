d51dd5024874903c260d5c9c631b402ec8e303e5
crispino480 <crispino480@users.noreply.github.com>
2020-12-18 00:15:06 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -43,9 +43,9 @@ while( fread(buffer,512, 1,inputptr) !=0) // Read blocks of 512 bytes, one at a 
             {
                isFound=1;
             }
-                sprintf(file_name, "%03i.jpg", count);
+                sprintf(file_name, "%03i.jpg", count++);
                 outptr = fopen(file_name, "w");
-                count++;
+               // count++;
 
                 if(isFound==1)
                 {
