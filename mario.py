972c3e67900e0f387acda910c4347222a11dddaf
crispino480 <crispino480@users.noreply.github.com>
2020-12-28 21:42:07 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,23 +1,21 @@
-
-#Write, in a file called mario.py in ~/pset6/mario/less/, 
-#a program that recreates the half-pyramid using hashes (#) for blocks
+# Write, in a file called mario.py in ~/pset6/mario/less/,
+# a program that recreates the half-pyramid using hashes (#) for blocks
 
 
 from cs50 import get_int
 
-height = get_int("Height: ")
 
-while height < 1 or height > 8:
-
+while True:
     height = get_int("Height: ")
-    
-for i in range(height) :
+    if 1 <= height and height <= 8:
+        break
 
-    for j in range(height+1) :
-        if j >= height-i :
-            print("#",end="")
+
+for i in range(height):
+    # print space and charater # from left to right
+    for j in range(height+1):
+        if j >= height-i:
+            print("#", end="")
         else:
-             print(" ",end="")
+            print(" ", end="")
     print()
-   
-
