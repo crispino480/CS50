e436056c9ee60d1b7295845b58c50fd80fe74cf6
crispino480 <crispino480@users.noreply.github.com>
2020-12-29 02:00:55 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -17,41 +17,42 @@ int main(void)
 {
 //prompt for a number for the pyramid height
 
-  int height;
+    int height;
 
-      do
+
+    do
     {
         printf("How tall is your pyramid\n");
         height = get_int("height: ");
 
     }
-      while (height < 1 || height > 8);
+    while (height < 1 || height > 8);
     // The input value is check to meet the requirement, otherwise the user must enter a value again
 
 // print the left side of the pyramid
     for (int i = 1 ; i <= height ; i++)
-      {
+    {
         for (int j = 1 ; j <= height ; j++)
-         {
+        {
             if (j > height - i)
             {
                 printf("#");
-                   }
-                        else
-                       {
-                 printf(" ");
-                    }
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         // allocate a space between the two pyramid
-         printf("  ");
-         //build the right side of the pyramid
+        printf("  ");
+        //build the right side of the pyramid
         for (int k = 1 ; k <= height ; k++)
         {
-             if (i < k)
-             {
+            if (i < k)
+            {
                 printf("");
-             }
-               else
+            }
+            else
             {
                 printf("#");
             }
