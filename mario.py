fb539690ec4cb02b2379f9565298742ca21fdf60
crispino480 <crispino480@users.noreply.github.com>
2020-12-29 01:09:51 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -5,14 +5,13 @@ while True:
     if 1 <= height and height <= 8:
         break
 
-for i in range(1,height+1):
+for i in range(1, height+1):
     # print space and charater # from left to right
-    for j in range(1,height+1):
-            if j > height -i:
-                print("#",end="")
-            else:
-                print(" ",end="")
-                
+    for j in range(1, height+1):
+        if j > height - i:
+            
+            print("#", end="")
+        else:
+            print(" ", end="")
+
     print()        
-  
-    
