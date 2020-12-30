10d56bade8e9888deab677566632d28e1c0b985b
crispino480 <crispino480@users.noreply.github.com>
2020-12-30 02:20:10 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -1,9 +1,10 @@
+#Implement a program that calculates the minimum number of coins required to give a user change.
 from cs50 import get_float
 
 def main():
 
-    ipt = get_input()   
-    cent = int((ipt*100))  
+    ipt = get_input()
+    cent = int((ipt*100))
     # output coin due
     print(f"Change owed :{changeCount(cent)}")
 
@@ -19,6 +20,6 @@ def get_input():
 
 def changeCount(c):
     return (c//25 + (c % 25)//10 + ((c % 25) % 10)//5 + ((c % 25) % 10) % 5 // 1)
-    
+
 
 main()
