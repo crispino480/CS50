a59ba1f5d670191658e4aeaf733c1c834838379d
crispino480 <crispino480@users.noreply.github.com>
2021-01-03 15:38:39 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -3,6 +3,7 @@
 from cs50 import get_string
 from cs50 import get_float
 
+
 def main():
 
     # To get user input
@@ -16,55 +17,51 @@ def main():
 
     index = round((0.0588*L - 0.296*Si - 15.8))
 
-    if index < 1 :
+    if index < 1:
         print(" Before Grade 1")
 
     elif index >= 16:
         print(" Grade 16+")
-    else :
+    else:
         print(f" Grade {index}")
-
-
 
 
 # this function count the number of sentences in a text
 def sentencecount(t):
-    counts=0
+    counts = 0
     for each_character in list(t):
-        if each_character in ['.','!','?']:
-            counts=counts +1
+        if each_character in ['.', '!', '?']:
+            counts = counts + 1
+
     return counts
 
 
 # this function count the number of words in a sentence
 def wordcount(t):
-    countw=1
+    countw = 1
     for each_character in list(t):
-        if each_character in [' '] :
+        if each_character in [' ']:
             # count letters
             countw = countw + 1
-    return  countw
-
+    return countw
 
 
 # this function count the number of letters in a sentence
 def lettercount(t):
-    countl=0
+    countl = 0
 
     for i in range(len(t)):
         # check if the input is a letter
         if t[i].isalpha():
             # count letters
-            countl=countl+1
+            countl = countl+1
     return countl
-
 
 
 # function to prompt for user input, return its value
 def userInput():
-    ipt = get_string (" Enter your text: ")
+    ipt = get_string(" Enter your text: ")
     return ipt
-
 
 
 main()
