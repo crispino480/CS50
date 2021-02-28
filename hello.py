7a6a72d6b871d3bec2b32a167d5fa60654aaabff
crispino480 <53025516+crispino480@users.noreply.github.com>
2021-02-27 21:58:36 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+# Written in Python
+# A program that prompts a user for their name, and then prints hello, so-and-so, where so-and-so is their provided name,
+
 from cs50 import get_string
 
 answer = get_string("What is your name ?\n")
