b7012cfdeeb07d5e1b629e355bb80198e63eccd3
crispino480 <53025516+crispino480@users.noreply.github.com>
2021-02-27 22:15:44 -0500

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,3 +1,4 @@
+# This program is written in Python
 # Implement a program that identifies a person based on their DNA, per the below.
 # $ python dna.py databases/large.csv sequences/5.txt
 # Lavender
